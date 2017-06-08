487950433f947a5e4956d1742abb0ba3979ed629
Amine Ghilazghi <aghilazghi@yahoo.com>
2017-06-08 17:25:55 -0400

    Added initial models and data context

---
+++ HabeshaCrafts/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace HabeshaCrafts.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
---
+++ HabeshaCrafts/Models/HabeshaCraftsContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace HabeshaCrafts.Models
+{
+    public class HabeshaCraftsContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
